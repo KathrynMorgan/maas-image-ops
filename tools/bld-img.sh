71619bc454fd2f27a7750c3ca72aeefc71131e03
Kat Morgan <kat.morgan@braincraft.io>
2018-10-20 00:00:05 +0000

    syntax fix

--- tools/bld-img.sh
+++ tools/bld-img.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-source_ISO=Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh.iso 
-#source_ISO=en_windows_server_2016_updated_feb_2018_x64_dvd_11636692.iso
+#source_ISO=Windows_Server_2016_Datacenter_EVAL_en-us_14393_refresh 
+source_ISO=en_windows_server_2016_updated_feb_2018_x64_dvd_11636692
 
 # RunCMD
 maas-image-builder \
   --vcpus "4" \
-  -o Server2016-amd64-root-dd windows \
-  --windows-iso ./iso/${source_ISO} \
+  -o ${source_ISO} windows \
+  --windows-iso ./iso/${source_ISO}.iso \
   --windows-edition win2016hv \
-  --windows-drivers /root/maas-img-builder/drivers/INF/ \
   --windows-updates \
+  --windows-drivers /root/maas-img-builder/drivers/INF/ \
   --windows-language en-US 
 
 # Refrence
